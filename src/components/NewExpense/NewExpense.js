6fd3eebfe90dbc40f5f910c7bf31c4359cf7c079
Carolina Maffia <carolinamaffia.m@gmail.com>
2021-06-16 13:32:29 -0300

    created ExpenseForm component

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import ExpenseForm from './ExpenseForm/ExpenseForm';
+import './newExpense.css';
+
+const NewExpense = () => {
+    return (
+        <div className="new-expense">
+            <ExpenseForm />
+        </div>
+    )
+};
+
+export default NewExpense;
