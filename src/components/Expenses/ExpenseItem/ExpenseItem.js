cd65181a992f39058fbf3200bd7f515f032ec49f
Carolina Maffia <carolinamaffia.m@gmail.com>
2021-06-22 14:43:00 -0300

    fixed child to parent

--- src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -1,16 +1,8 @@
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import Card from '../../UI/Card/Card';
-
 import './expenseItem.css';
-import { useState } from 'react';
 
 const ExpenseItem = (props) => {
-
-    const [title, setTitle] = useState(props.title);
-    
-    const handleClick = () => {
-         setTitle('Updated!');   
-    }
 
     return (
         <Card className="expense-item">
@@ -18,10 +10,9 @@ const ExpenseItem = (props) => {
                 <ExpenseDate date={props.date} />
             </div>
             <div>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
             </div>
             <div className="expense-item__price">${props.amount}</div>
-            <button onClick={handleClick}>Change title</button>
         </Card>
     )
 }
